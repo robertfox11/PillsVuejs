6a5e015749047f3e8adc80406867a9f7c38025c0
robert Salazar <11rsahome@gmail.com>
2020-02-21 18:04:10 +0100

    api img

--- vuejs/api.js
+++ vuejs/api.js
@@ -1,10 +1,12 @@
 Vue.component('common-list',{
   props : ['lists'],
-  template : `<ul class="list-group">
-  <li v-for="item in lists" class="list-group-item">
-      {{item.title}}
-  </li>
-</ul>`
+  template : ` <div class="row">
+  <div class="card col-4 m-1 mx-auto" v-for="item in lists">
+    <div class="card-body">
+      <h5 class="card-title" >{{ item.id}}</h5>
+      <p class="card-text">{{ item.title}}</p>
+    </div>
+  </div>`
 });
 var urlPhotos = "http://jsonplaceholder.typicode.com/photos?_limit=5&_page=1";
 new Vue({
